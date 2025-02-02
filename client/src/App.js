1efabd5f9e4fa4e2774830fe69fd7706fc4acd1e
Rosa M Perez <rosaperezlarios@gmail.com>
2025-02-02 14:02:50 -0800

    Form outputs data that is typed in the handle change of the console

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    title: '',
+    body: '',
+    posts: []
+  };
 
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+  
+    this.setState({ 
+      [name]: value 
+    });
+  };
+
+  // handleChange = ({ target }) => {
+  //   const { name, value } = target;
+  //   this.setState({ [name]: value });
+  // };
+
+  render() {
+    console.log('State: ', this.state);
+    // JSX
+    return (
+      <div>
+        <h2>Welcome to my Blog app</h2>
+        <form>
+        <div className="form-input">
+        <input 
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={this.state.title}
+              // onChange={(e) => console.log(e.target.value)}
+              onChange={this.handleChange}
+
+
+            />
+        </div>
+        <div className="form-input"></div>
+        <textarea
+              placeholder="body"
+              name="body"
+              cols="30"
+              rows="10"
+              value={this.state.body}
+              onChange={this.handleChange}
+            >
+            </textarea>
+            <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
 export default App;
