9c137a47bd54b0a4c3ce934e14b42db08679cffc
Rosa M Perez <rosaperezlarios@gmail.com>
2025-02-02 20:33:49 -0800

    Data being stored inside our state

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,23 @@ class App extends React.Component {
     posts: []
   };
 
+  componentDidMount = () => {
+    this.getBlogPost();
+  };
+  
+  getBlogPost = () => {
+    axios.get('/api')
+      .then((response) => {
+        const data = response.data;
+        this.setState({ posts: data });
+        console.log('Data has been received!!');
+      })
+      .catch(() => {
+        alert('Error retrieving data!!!');
+      });
+  }
+
+
   handleChange = ({ target }) => {
     const { name, value } = target;
     this.setState({ [name]: value });
